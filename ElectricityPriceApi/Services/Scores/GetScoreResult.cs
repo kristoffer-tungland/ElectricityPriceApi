66d80f93ff0902a015a7146727dc5808fb9c204a
Kristoffer Tungland <ktu@cowi.com>
2022-09-13 12:33:37 +0200

    Added score and price fore each hour

--- ElectricityPriceApi/Services/Scores/GetScoreResult.cs
+++ ElectricityPriceApi/Services/Scores/GetScoreResult.cs
@@ -5,14 +5,24 @@ namespace ElectricityPriceApi.Services.Scores;
 
 public class GetScoreResult
 {
-    public GetScoreResult(int scoreNow, int hour, List<HourPriceScore> prices)
+    public GetScoreResult(int scoreNow, int hourNow, List<HourPriceScore> prices)
     {
         ScoreNow = scoreNow;
-        Hour = hour;
+        HourNow = hourNow;
         Prices = prices;
+
+        foreach (var hourPriceScore in prices)
+        {
+            HourScores.Add($"ScoreOfHour{hourPriceScore.Time.Hour}", hourPriceScore.Score);
+            HourPrices.Add($"PriceOfHour{hourPriceScore.Time.Hour}", hourPriceScore.Price);
+        }
     }
 
     public int ScoreNow { get; }
-    public int Hour { get; }
+    public int HourNow { get; }
+    
+    public Dictionary<string, int> HourScores { get; } = new();
+    public Dictionary<string, float> HourPrices { get; } = new();
+
     public List<HourPriceScore> Prices { get; }
 }
