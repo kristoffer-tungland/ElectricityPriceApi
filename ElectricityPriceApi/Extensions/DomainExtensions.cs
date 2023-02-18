faa7acdfee8cbdbd11dffa1fc4cf8d1cf5c8674b
Kristoffer Tungland <ktu@cowi.com>
2023-02-18 21:58:07 +0100

    Fixed #6 and updated build scripts

--- ElectricityPriceApi/Extensions/DomainExtensions.cs
+++ ElectricityPriceApi/Extensions/DomainExtensions.cs
@@ -4,13 +4,14 @@ public static class DomainExtensions
 {
     public static string ConvertToDomain(this Area area)
     {
+        // Link to area codes https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html#_areas
         return area switch
         {
             Area.No1 => "10YNO-1--------2",
             Area.No2 => "10YNO-2--------T",
             Area.No3 => "10YNO-3--------J",
             Area.No4 => "10YNO-4--------9",
-            Area.No5 => "10YNO-0--------C",
+            Area.No5 => "10Y1001A1001A48H",
             _ => throw new ArgumentOutOfRangeException(nameof(area), area, null)
         };
     }
